45355f68ca7763f770e4ac8bb2ca7b707be38d13
etriv <itai.riv@gmail.com>
2020-03-26 18:25:30 +0100

    Add test to check if warning msg is displayed when search is invalid

--- src/App.test.js
+++ src/App.test.js
@@ -4,15 +4,14 @@ import { render, fireEvent, cleanup } from '@testing-library/react';
 import { mockCharacters, mockEpisodes } from './modules/mock-data';
 import { getEpisodes, getAllCharactersInPage } from './modules/rick-manager';
 
+// Mocking API calls
 jest.mock('./modules/rick-manager');
+getAllCharactersInPage.mockResolvedValue({ characters: mockCharacters, numOfPages: 1 });
+getEpisodes.mockResolvedValue(mockEpisodes);
 
 afterEach(cleanup);
 
 it('Shows results when searching for existing character', async () => {
-  // Mocking API calls
-  getAllCharactersInPage.mockResolvedValue({ characters: mockCharacters, numOfPages: 1 });
-  getEpisodes.mockResolvedValue(mockEpisodes);
-
   const { getByText, getByPlaceholderText, debug, findByText } = render(<App />);
 
   fireEvent.change(getByPlaceholderText(/Find a character/i), { target: { value: 'rick' } });
@@ -35,4 +34,16 @@ it('Shows results when searching for existing character', async () => {
   // Making sure species recommendations are being displayed:
   expect(getByText(/Summer Smith/i)).toBeInTheDocument();
   expect(getByText(/Albert Einstein/i)).toBeInTheDocument();
+});
+
+it('Shows a warning message when searching for a non-existing character', async () => {
+  const { getByText, getByPlaceholderText, debug, findByText } = render(<App />);
+
+  fireEvent.change(getByPlaceholderText(/Find a character/i), { target: { value: 'non-existing character' } });
+  const searchButton = getByText('SEARCH');
+  fireEvent.click(searchButton);
+
+  // Making sure episodes are being displayed
+  const warningMsg = await findByText(/choose an existing character/i);
+  expect(warningMsg).toBeInTheDocument();
 });
